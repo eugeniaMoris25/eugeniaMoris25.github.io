0287996976fb8fbbec8fdfea97cd84cac99db3be
eugeniaMoris25 <150850619+eugeniaMoris25@users.noreply.github.com>
2024-05-14 14:21:37 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,34 +1,14 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+const menuItems = document.querySelectorAll('.nav-item');
+const mainContent = document.querySelector('.main-content');
 
-window.addEventListener('DOMContentLoaded', event => {
+menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', () => {
+        const sectionId = menuItem.getAttribute('data-section-id');
+        const section = document.getElementById(sectionId);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
+        mainContent.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
         });
     });
-
 });
